ff585d74c350f9cc0b9c9dcdcb5f1bfa1b038e06
rodrigues <marcos.rodrigues@sh3.com.br>
2024-10-23 14:58:44 -0300

    Portfolio

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,28 +1,42 @@
 <template>
-    <div class="bg-white shadow-md rounded-lg p-4">
-      <h3 class="text-lg font-semibold mb-2">{{ title }}</h3>
-      <p class="text-gray-700 mb-2">{{ description }}</p>
-      <a class="text-gray-900 font-bold" href="{{ link }}">GitHub</a>
-      <slot />
-    </div>
-  </template>
-  
-  <script setup>
-  import { defineProps } from 'vue';
-  
-  const props = defineProps({
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    link: {
-      type: String,
-      required: true,
-    },
-  });
-  </script>
-  
+  <div class="bg-white shadow-lg rounded-lg p-6 transition-transform transform hover:scale-105 hover:shadow-xl cursor-pointer	">
+    <h3 class="text-xl font-bold text-gray-800 mb-3">{{ title }}</h3>
+    <p class="text-gray-600 mb-4">{{ description }}</p>
+    <a class="text-blue-600 font-semibold hover:underline" href="{{ link }}">GitHub</a>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+/* Adicionando algumas sombras e transições */
+.bg-white {
+  transition: all 0.3s ease;
+}
+
+.hover\:scale-105:hover {
+  transform: scale(1.05);
+}
+
+.hover\:shadow-xl:hover {
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.15);
+}
+</style>
